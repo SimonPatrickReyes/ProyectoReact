f6dbc0fac4c78d760f4b862d8b15c989b32472ad
simon.patrick <simonpatrickreyes@gmail.com>
2023-06-12 21:35:29 +0200

    update login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,78 +1,86 @@
-import React, { useState } from 'react';
+import React, { useState, useContext  } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { UserContext } from '../contexts/UserContext';
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
-  const [formSend, changeFormSend] = useState(false)
+  const [formSend, changeFormSend] = useState(false);
+  const users = useState(JSON.parse(localStorage.getItem('users')) || [])
+  const [failed, setFailed] = useState(false);
+  const { user, setUser } = useContext(UserContext)
+  const navigate = useNavigate()
+
+
+
   return (
     <>
       <Formik
         initialValues={{
-          name: '',
           email: '',
           password: ''
         }}
 
-        validate={(valores)=>{
-          let errors ={};
-
-          //validar name de usuario
-          if (!valores.name) {
-            errors.name='Write a username'
-          } else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.name)){
-            errors.name = "Username can only contain letters and spaces"
-          }
+        validate={(valores) => {
+          let errors = {};
 
           //validar email
           if (!valores.email) {
-            errors.email='Write an email'
-          } else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.email)){
+            errors.email = 'Write an email'
+          } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.email)) {
             errors.email = "The email can only contain letters, numbers, periods, hyphens and underscores"
           }
-          return errors 
+          return errors
         }}
 
-        onSubmit={(valores,{resetForm}) => {
-          resetForm();
+        onSubmit={(valores, { resetForm }) => {
           console.log('Form send');
-          changeFormSend(true)
+          setFailed(false)
+
+          var userRegistered = users[0].filter(user => user.email == valores.email && user.password == valores.password)[0]
+          console.log(users[0])
+          console.log(userRegistered)
+
+          if (userRegistered != null) {
+            setUser(true) //UserContext
+            changeFormSend(true);
+
+            window.localStorage.setItem("email", userRegistered.email);
+            window.localStorage.setItem("password", userRegistered.password);
+            resetForm();
+            navigate('/')
+          } else {
+            setFailed(true)
+          }
+
+
         }}
       >
         {({ errors }) => (
           <Form className="formLogin">
             <div>
-              <label htmlFor="name">Username</label>
-              <Field 
-              type="text" 
-              id="name" 
-              name="name"
-              className="formLogin__field"/>
-              <ErrorMessage name='name' component={()=>(
-                <div className='error'>{errors.name}</div>
-              )}
-              />
-            </div>
-            <div>
               <label htmlFor="email">Email</label>
-              <Field 
-              type="email"
-               id="email" 
-               name="email" 
-               className="formLogin__field"/>
-               <ErrorMessage name='email' component={()=>(
+              <Field
+                type="email"
+                id="email"
+                name="email"
+                className="formLogin__field" />
+              <ErrorMessage name='email' component={() => (
                 <div className='error'>{errors.email}</div>
               )}
               />
             </div>
             <div>
               <label htmlFor="password">Password</label>
-              <Field 
-              type="password" 
-              id="password" 
-              name="password" 
-              className="formLogin__field"/>
+              <Field
+                type="password"
+                id="password"
+                name="password"
+                className="formLogin__field" />
             </div>
             <button type="submit">Log in</button>
-            {formSend && <span className='login__span'>Successfully logged</span>} 
+            {formSend && <span className='login__span'>Successfully logged</span>}
+            {failed && <span>Check again your credentials</span>}
             <a href="http://localhost:5173/register">Not registered yet?</a>
           </Form>
         )}
